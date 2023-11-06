2d800d60f4e35efe6d08513c7cebd165c6b48e34
GageHoweGK <howe.gaged@gmail.com>
2023-11-05 20:21:45 -0600

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -1,78 +1,75 @@
-/* import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
-
-const canvas = document.querySelector('.webgl');
-const scene = new THREE.Scene();
-
-
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({
-    color: 0xff0000
-});
-
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
-
-
-
-
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
-
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
-camera.position.set(0, 0, 3);
-scene.add(camera);
-
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-});
-
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-renderer.shadowMap.enabled = true;
-renderer.render(scene, camera);
-
-function animate() {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-}
-
-animate();  */
-
 import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
+import { AsciiEffect } from 'three/addons/effects/AsciiEffect.js';
+import { TrackballControls } from 'three/addons/controls/TrackballControls.js';
 
 const canvas = document.querySelector('.webgl');
 const scene = new THREE.Scene();
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({
-    color: 0xff0000
-});
+let camera, controls, renderer, effect;
 
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+let sphere, plane;
 
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+const start = Date.now();
+
+init();
+animate();
+
+function init() {
+    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
+    camera.position.y = 150;
+    camera.position.z = 500;
+
+    scene.background = new THREE.Color(0, 0, 0);
+
+    const pointLight1 = new THREE.PointLight(0xffffff, 3, 0, 0);
+    pointLight1.position.set(500, 500, 500);
+    scene.add(pointLight1);
+
+    const pointLight2 = new THREE.PointLight(0xffffff, 1, 0, 0);
+    pointLight2.position.set(-500, -500, -500);
+    scene.add(pointLight2);
+
+    sphere = new THREE.Mesh(new THREE.SphereGeometry(200, 20, 10), new THREE.MeshPhongMaterial({ flatShading: true }));
+    scene.add(sphere);
+
+    // Plane
+    plane = new THREE.Mesh(new THREE.PlaneGeometry(400, 400), new THREE.MeshBasicMaterial({ color: 0xe0e0e0 }));
+    plane.position.y = -200;
+    plane.rotation.x = -Math.PI / 2;
+    scene.add(plane);
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    effect = new AsciiEffect(renderer, ' .:-+*=%@#', { invert: true });
+    effect.setSize(window.innerWidth, window.innerHeight);
+    effect.domElement.style.color = 'white';
+    effect.domElement.style.backgroundColor = 'black';
+
+    document.body.appendChild(effect.domElement);
+
+    controls = new TrackballControls(camera, effect.domElement);
+
+    window.addEventListener('resize', onWindowResize);
 }
 
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
-camera.position.set(0, 0, 3);
-scene.add(camera);
-
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-});
-
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-renderer.shadowMap.enabled = true;
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    effect.setSize(window.innerWidth, window.innerHeight);
+}
 
 function animate() {
     requestAnimationFrame(animate);
-    renderer.render(scene, camera);
+    render();
 }
 
-animate();
+function render() {
+    const timer = Date.now() - start;
+    sphere.position.y = Math.abs(Math.sin(timer * 0.002)) * 150;
+    sphere.rotation.x = timer * 0.0003;
+    sphere.rotation.z = timer * 0.0002;
+    controls.update();
+    effect.render(scene, camera);
+}
